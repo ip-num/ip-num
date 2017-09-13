f673c53c012d882f9e82e48310149f8af8372445
Dadepo Aderemi <dadepo@gmail.com>
2017-09-13 23:14:37 +0200

    Adding the version number in the file. Started tracking the release version in git

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var webpack = require("webpack");
-var releaseVersion = require("./package.json").version;
+var WebpackAutoInject = require('webpack-auto-inject-version');
 
 module.exports = {
   entry: './dist/src/index.js',
   output: {
-    filename: 'ip-num-' + releaseVersion + ".js",
+    filename: 'ip-num.js',
     library: 'ipnum'
   },
   devtool: "source-map",
@@ -12,6 +12,21 @@ module.exports = {
     extensions: ['.webpack.js', '.web.js', '.ts', '.js']
   },
   target: "web",
+  plugins: [
+    new WebpackAutoInject({
+      SHORT: 'ip-num',
+      components: {
+        InjectAsComment: true,
+        AutoIncreaseVersion: false
+      },
+      componentsOptions: {
+        InjectAsComment: {
+          tag: 'Version: {version}. Released on: {date}',
+          dateFormat: 'dddd, mmmm dS, yyyy, h:MM:ss TT'
+          }
+      }
+    })
+  ],
   module: {
     loaders: [
       { test: /\.ts$/, loader: 'awesome-typescript-loader' },
