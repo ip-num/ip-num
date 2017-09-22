314d0ed4f55a2ae74dda4f2bdcfb2e8ab3cc702f
Dadepo Aderemi <dadepo@gmail.com>
2017-09-22 17:20:15 +0200

    Updated the test for Prefix module

--- src/Prefix.ts
+++ src/Prefix.ts
@@ -6,13 +6,36 @@ import {IPv6Subnet} from "./Subnet";
 import {binaryStringToHexadecimalString} from "./HexadecimalUtils";
 import {Hexadecatet} from "./Hexadecatet";
 
+/**
+ * Represents the prefix portion in the CIDR notation for representing IP ranges
+ *
+ * The IPv4 prefix portion represents the subnet mask. It is the number of continuous bits turned on (with value 1)
+ * counting from the left side of an 8 bit value.
+ *
+ * {@see https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing} for more information on CIDR
+ */
 class IPv4Prefix {
+    /**
+     * The decimal value of the 8bit number representing the prefix
+     */
     value: number;
 
+    /**
+     * Convenience method for constructing an instance of IPv4 prefix from a decimal number
+     *
+     * @param {number} rawValue the decimal value to construct the IPv4 prefix from.
+     * @returns {IPv4Prefix} the instance of an IPv4 prefix
+     */
     static fromNumber(rawValue:number):IPv4Prefix {
         return new IPv4Prefix(rawValue);
     };
 
+    /**
+     * Constructor for an instance of IPv4 prefix from a decimal number
+     *
+     * @param {number} rawValue the decimal value to construct the IPv4 prefix from.
+     * @returns {IPv4Prefix} the instance of an IPv4 prefix
+     */
     constructor(rawValue: number) {
         let isValid: boolean;
         let message: string[];
@@ -23,14 +46,30 @@ class IPv4Prefix {
         this.value = rawValue;
     }
 
+    /**
+     * Gets the decimal value of the IPv4 prefix
+     *
+     * @returns {number} the decimal value of the IPv4 prefix
+     */
     public getValue(): number {
         return this.value;
     }
 
+    /**
+     * Gets the decimal value of the IPv4 prefix as string
+     * @returns {string} he decimal value of the IPv4 prefix as string
+     */
     public toString(): string {
         return this.value.toString();
     }
 
+    /**
+     * Converts the IPv4 prefix to a {@link IPv4Subnet}
+     *
+     * The IPv4 Subnet is the representation of the prefix in the dot-decimal notation
+     *
+     * @returns {IPv4Subnet} the subnet representation of the IPv4 number
+     */
     public toSubnet(): IPv4Subnet {
         let onBits = '1'.repeat(this.value);
         let offBits = '0'.repeat(32 - this.value);
@@ -42,13 +81,36 @@ class IPv4Prefix {
     }
 }
 
+/**
+ * Represents the prefix portion in the CIDR notation for representing IP ranges
+ *
+ * The IPv6 prefix portion represents the subnet mask. It is the number of continuous bits turned on (with value 1)
+ * counting from the left side of an 128 bit value.
+ *
+ * {@see https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing} for more information on CIDR
+ */
 class IPv6Prefix {
+    /**
+     * The decimal value of the 16bit number representing the prefix
+     */
     value: number;
 
+    /**
+     * Convenience method for constructing an instance of IPv46 prefix from a decimal number
+     *
+     * @param {number} rawValue the decimal value to construct the IPv6 prefix from.
+     * @returns {IPv4Prefix} the instance of an IPv6 prefix
+     */
     static fromNumber(rawValue:number):IPv6Prefix {
         return new IPv6Prefix(rawValue);
     };
 
+    /**
+     * Constructor for an instance of IPv6 prefix from a decimal number
+     *
+     * @param {number} rawValue the decimal value to construct the IPv6 prefix from.
+     * @returns {IPv4Prefix} the instance of an IPv6 prefix
+     */
     constructor(rawValue: number) {
         let isValid: boolean;
         let message: string[];
@@ -59,14 +121,30 @@ class IPv6Prefix {
         this.value = rawValue;
     }
 
+    /**
+     * Gets the decimal value of the IPv6 prefix
+     *
+     * @returns {number} the decimal value of the IPv6 prefix
+     */
     public getValue(): number {
         return this.value;
     }
 
+    /**
+     * Gets the decimal value of the IPv4 prefix as string
+     * @returns {string} he decimal value of the IPv4 prefix as string
+     */
     public toString(): string {
         return this.value.toString();
     }
 
+    /**
+     * Converts the IPv6 prefix to a {@link IPv6Subnet}
+     *
+     * The IPv6 Subnet is the representation of the prefix in 8 groups of 16 bit values represented in hexadecimal
+     *
+     * @returns {IPv4Subnet} the subnet representation of the IPv4 number
+     */
     public toSubnet(): IPv6Subnet {
         let onBits = '1'.repeat(this.value);
         let offBits = '0'.repeat(128 - this.value);
