a8b511f21c106807e92d41f3c8d9a8e3055ee39f
Dadepo Aderemi <dadepo@gmail.com>
2017-09-29 20:53:24 +0200

    Going with the prerelease semantic of semvar

--- docs/ip-num.js
+++ docs/ip-num.js
@@ -1,4 +1,4 @@
-// [ip-num]  Version: 0.0.12-pre-alpha. Released on: Thursday, September 28th, 2017, 1:35:01 PM  
+// [ip-num]  Version: 1.0.0-0. Released on: Friday, September 29th, 2017, 8:49:57 PM  
  var ipnum =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
