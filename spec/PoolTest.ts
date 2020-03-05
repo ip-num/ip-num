7ed00fb159941c7f39363f339d00a4eaf008fa46
Dadepo Aderemi <dadepo@gmail.com>
2020-03-05 12:06:45 +0100

    Updated tests for IPPool

--- spec/PoolTest.ts
+++ spec/PoolTest.ts
@@ -1,4 +1,4 @@
-import {IPv4, IPv4CidrRange, IPv6, Range} from "../src";
+import {IPv4, IPv4CidrRange, IPv6, IPv6CidrRange, Range} from "../src";
 import {Pool} from "../src/IPPool";
 
 describe('Pool', () => {
@@ -16,6 +16,7 @@ describe('Pool', () => {
             let aggregatedPool = pool.aggregate();
 
             expect(aggregatedPool.getRanges()[0].toRangeString()).toEqual("192.168.0.0-192.168.0.255");
+            expect(aggregatedPool.getRanges().length).toEqual(1);
         });
 
         it('should aggregate with hole', () => {
@@ -34,6 +35,7 @@ describe('Pool', () => {
             expect(aggregatedPool.getRanges()[0].toRangeString()).toEqual("192.168.0.0-192.168.0.127");
             expect(aggregatedPool.getRanges()[1].toRangeString()).toEqual("192.168.0.160-192.168.0.191");
             expect(aggregatedPool.getRanges()[2].toRangeString()).toEqual("192.168.0.192-192.168.0.255");
+            expect(aggregatedPool.getRanges().length).toEqual(3);
         });
 
         it('should aggregate with the whole space', () => {
@@ -51,6 +53,54 @@ describe('Pool', () => {
             let aggregatedPool = pool.aggregate();
 
             expect(aggregatedPool.getRanges()[0].toRangeString()).toEqual("0.0.0.0-255.255.255.255");
+            expect(aggregatedPool.getRanges().length).toEqual(1);
+        });
+    });
+
+    describe("IPv6", () => {
+        it("it should fully aggregate", () => {
+            let arrays: Range<IPv4 | IPv6>[] = new Array<Range<IPv6>>();
+
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:0:0:0:0:0:0/48")));
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:0:0:0:0:0/50")));
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:4000:0:0:0:0/50")));
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:8000:0:0:0:0/49")));
+
+            let pool = Pool.fromIPRanges(arrays);
+            let aggregatedPool = pool.aggregate();
+            expect(aggregatedPool.getRanges()[0].toRangeString()).toEqual("2001:db8:0:0:0:0:0:0-2001:db8:1:ffff:ffff:ffff:ffff:ffff");
+            expect(aggregatedPool.getRanges().length).toEqual(1);
+        });
+
+        it('should aggregate with hole', () => {
+            let arrays: Range<IPv4 | IPv6>[] = new Array<Range<IPv6>>();
+
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:0:0:0:0:0:0/48")));
+            // arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:0:0:0:0:0/50"))); - hole
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:4000:0:0:0:0/50")));
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:8000:0:0:0:0/49")));
+
+            let pool = Pool.fromIPRanges(arrays);
+            let aggregatedPool = pool.aggregate();
+            expect(aggregatedPool.getRanges()[0].toRangeString()).toEqual("2001:db8:0:0:0:0:0:0-2001:db8:0:ffff:ffff:ffff:ffff:ffff");
+            expect(aggregatedPool.getRanges()[1].toRangeString()).toEqual("2001:db8:1:4000:0:0:0:0-2001:db8:1:7fff:ffff:ffff:ffff:ffff");
+            expect(aggregatedPool.getRanges()[2].toRangeString()).toEqual("2001:db8:1:8000:0:0:0:0-2001:db8:1:ffff:ffff:ffff:ffff:ffff");
+            expect(aggregatedPool.getRanges().length).toEqual(3);
+        });
+
+        it('should aggregate with the whole space', () => {
+            let arrays: Range<IPv4 | IPv6>[] = new Array<Range<IPv6>>();
+
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("::0/0")));
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:0:0:0:0:0:0/48")));
+            // arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:0:0:0:0:0/50"))); - hole
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:4000:0:0:0:0/50")));
+            arrays.push(Range.fromCidrRange(IPv6CidrRange.fromCidr("2001:db8:1:8000:0:0:0:0/49")));
+
+            let pool = Pool.fromIPRanges(arrays);
+            let aggregatedPool = pool.aggregate();
+            expect(aggregatedPool.getRanges()[0].toRangeString()).toEqual("::0-ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
+            expect(aggregatedPool.getRanges().length).toEqual(1);
         });
     });
 });
