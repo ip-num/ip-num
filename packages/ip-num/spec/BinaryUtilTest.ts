a638902a69e13f1b0b956274fcb05d44adb975ba
Dadepo Aderemi <dadepo@gmail.com>
2021-12-28 14:24:03 +0100

    Added pbt to BinaryUtilTest

--- packages/ip-num/spec/BinaryUtilTest.ts
+++ packages/ip-num/spec/BinaryUtilTest.ts
@@ -7,27 +7,27 @@ import {decimalAndBinary, ipv4DecimalNotation} from "./arbitraties/BinaryArbitra
 import {min} from "big-integer";
 
 describe('Binary Utils', () => {
-    fit('Should correctly convert decimal to binary', () => {
+    it('Should correctly convert decimal to binary', () => {
         fc.assert(fc.property(decimalAndBinary, (value) => {
             expect(BinaryUtils.decimalNumberToBinaryString(value.value)).toEqual(value.binary);
         }));
     });
-    fit('Should correctly parse binary string to a number in BigInteger', () => {
+    it('Should correctly parse binary string to a number in BigInteger', () => {
         fc.assert(fc.property(decimalAndBinary, (value) => {
             expect(BinaryUtils.parseBinaryStringToBigInteger(value.binary)).toEqual(bigInt(value.value));
         }));
     });
-    fit('Should correctly convert binary to decimal', () => {
+    it('Should correctly convert binary to decimal', () => {
         fc.assert(fc.property(decimalAndBinary, (value) => {
             expect(BinaryUtils.parseBinaryStringToBigInteger(value.binary).valueOf()).toEqual(value.value);
         }));
     });
-    fit('Should correctly convert a big integer number to binary string', () => {
+    it('Should correctly convert a big integer number to binary string', () => {
         fc.assert(fc.property(decimalAndBinary, (value) => {
             expect(BinaryUtils.bigIntegerNumberToBinaryString(bigInt(value.value))).toEqual(value.binary);
         }));
     });
-    fit('Should correctly convert binary to decimal and back to binary', () => {
+    it('Should correctly convert binary to decimal and back to binary', () => {
         fc.assert(fc.property(decimalAndBinary, (value) => {
             expect(
                 BinaryUtils.decimalNumberToBinaryString(
@@ -36,7 +36,7 @@ describe('Binary Utils', () => {
             ).toEqual(value.binary);
         }));
     });
-    fit('Should correctly convert decimal number to octets', () => {
+    it('Should correctly convert decimal number to octets', () => {
         fc.assert(fc.property(decimalAndBinary.filter(value => {
             return value.binary.length <= 8;
         }).map(value => {
@@ -48,17 +48,17 @@ describe('Binary Utils', () => {
             expect(BinaryUtils.decimalNumberToOctetString(value.value)).toEqual(value.binary);
         }));
     });
-    fit('Should throw an exception when converting to octet and value is larger than an octet', () => {
+    it('Should throw an exception when converting to octet and value is larger than an octet', () => {
         expect(() => {
             BinaryUtils.decimalNumberToOctetString(122222222222);
         }).toThrowError(Error, 'Given decimal in binary contains digits greater than an octet');
     });
-    fit('Should correctly convert IP number in dotted decimal notation to binary string', () => {
+    it('Should correctly convert IP number in dotted decimal notation to binary string', () => {
         fc.assert(fc.property(ipv4DecimalNotation, (value) => {
             expect(BinaryUtils.dottedDecimalNotationToBinaryString(value.decimalNotation)).toEqual(value.binary);
         }));
     });
-    fit('Should pad given string with zeros to become given length', () => {
+    it('Should pad given string with zeros to become given length', () => {
         fc.assert(fc.property(fc.tuple(fc.integer({min:0}).map((value: number) => {
             return value.toString(2)
         }), fc.integer({min:0, max:20})).filter(values => {
@@ -75,7 +75,7 @@ describe('Binary Utils', () => {
             BinaryUtils.leftPadWithZeroBit('111111110', 5)
         }).toThrowError(Error, 'Given string is already longer than given final length after padding: 5');
     });
-    fdescribe('IPv4 cidr prefix to binary string', () => {
+    describe('IPv4 cidr prefix to binary string', () => {
       it('should convert prefix to mask binary string', () => {
           fc.assert(fc.property(fc.integer(1,32), (value) => {
               let maskString = BinaryUtils.cidrPrefixToMaskBinaryString(value, IPNumType.IPv4)
@@ -89,23 +89,31 @@ describe('Binary Utils', () => {
       });
     });
     describe('IPv6 cidr prefix to binary string', () => {
-      it('should convert a 64 prefix', () => {
-      expect(BinaryUtils.cidrPrefixToMaskBinaryString(64, IPNumType.IPv6)).toBe(`${"1".repeat(64)}${"0".repeat(64)}`)
-      });
-      it('should convert a 128 prefix', () => {
-        expect(BinaryUtils.cidrPrefixToMaskBinaryString(128, IPNumType.IPv6)).toBe(`${"1".repeat(128)}`)
-      });
-      it('should throw an exception when converting 130 prefix', () => {
-        expect(() => {
-        BinaryUtils.cidrPrefixToMaskBinaryString(130, IPNumType.IPv6);
-        }).toThrowError(Error, 'Value is greater than 128');
-      });
+
+        it('should convert prefix to mask binary string', () => {
+            fc.assert(fc.property(fc.integer(1, 128), (value) => {
+                let maskString = BinaryUtils.cidrPrefixToMaskBinaryString(value, IPNumType.IPv6)
+                expect(new RegExp(`^1{${value}}0{${128-value}}$`).test(maskString)).toBeTrue()
+            }))
+        });
+
+        it('should throw an exception when converting 130 prefix', () => {
+            expect(() => {
+                BinaryUtils.cidrPrefixToMaskBinaryString(130, IPNumType.IPv6);
+            }).toThrowError(Error, 'Value is greater than 128');
+        });
     });
 
     describe('log2', () => {
         it('should calculate the log2 of a number', () => {
-            expect(intLog2(bigInt(8))).toBe(3);
-            expect(intLog2(bigInt(256))).toBe(8);
+
+            fc.assert(fc.property(fc.integer(2, 100).map(value => {
+                return Math.pow(2, value);
+            }), (value) => {
+                let exponent = intLog2(bigInt(value));
+                expect(Math.pow(2, exponent)).toEqual(value)
+            }))
+
         });
 
         it('should throw an exception when no int log2', () => {
