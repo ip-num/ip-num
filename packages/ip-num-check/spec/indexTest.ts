409d07cbe15ac5cd3b4d9075ede82cc10e3b1914
Dadepo Aderemi <dadepo@gmail.com>
2021-12-31 00:57:32 +0100

    Added arbitrary for ipv4

--- packages/ip-num-check/spec/indexTest.ts
+++ packages/ip-num-check/spec/indexTest.ts
@@ -1,17 +1,30 @@
 import ipcheck from "../src";
 import fc from "fast-check";
-import {Asn} from "ip-num";
+import {Asn, IPv4} from "ip-num";
 
 describe('ASNs', () => {
-    it('Generates Arbitrary between 0 and 65535', () => {
+    it('Generates arbitrary between 0 and 65535', () => {
         fc.assert(fc.property(ipcheck.asn(), (value: Asn) => {
             return value.value.valueOf() >= 0 || value.value.valueOf() <= 65535
         }))
     });
-    it('Generates Arbitrary between given min and max', () => {
+    it('Generates arbitrary between given min and max', () => {
         fc.assert(fc.property(ipcheck.asn(2,3), (value: Asn) => {
             return value.value.valueOf() >= 2 || value.value.valueOf() <= 3
         }))
     });
+})
+
+describe('IPv4', () => {
+    it('Generates arbitrary values between 0 and 2^32 - 1', () => {
+        fc.assert(fc.property(ipcheck.ipv4(), (value: IPv4) => {
+            return value.value.valueOf() >= 0 || value.value.valueOf() <= (Math.pow(2,32) - 1)
+        }))
+    })
+    it('Generates arbitrary values between min and max', () => {
+        fc.assert(fc.property(ipcheck.ipv4(2,5), (value: IPv4) => {
+            return value.value.valueOf() >= 2 || value.value.valueOf() <= 5
+        }))
+    })
 })
 
--- packages/ip-num-check/src/index.ts
+++ packages/ip-num-check/src/index.ts
@@ -1,12 +1,20 @@
 import fc from "fast-check"
-import {Asn} from "ip-num";
+import {Asn, IPv4} from "ip-num";
+import * as bigInt from "big-integer/BigInteger";
+
+const ipv4max = Math.pow(2,32) - 1
 
 const asn = (min:number=0, max:number=65535) => {
     return fc.integer({min, max}).map(value => Asn.fromNumber(value))
 }
 
+const ipv4 = (min:number=0,max:number=ipv4max) => {
+    return fc.integer({min,max}).map(value => IPv4.fromBigInteger(bigInt(value)))
+}
+
 const ipcheck = {
     asn,
+    ipv4
 }
 
 export default ipcheck;
