6bfbcf7850dde6a2474a25a936f763f42b0d2523
Dadepo Aderemi <dadepo@gmail.com>
2022-01-01 18:23:21 +0100

    Adding arbirary for IPv6

--- packages/ip-num-check/spec/indexTest.ts
+++ packages/ip-num-check/spec/indexTest.ts
@@ -1,6 +1,6 @@
 import ipcheck from "../src";
 import fc from "fast-check";
-import {Asn, IPv4} from "ip-num";
+import {Asn, IPv4, IPv6, Validator} from "ip-num";
 
 describe('ASNs', () => {
     it('Generates arbitrary between 0 and 65535', () => {
@@ -24,6 +24,19 @@ describe('IPv4', () => {
     it('Generates arbitrary values between min and max', () => {
         fc.assert(fc.property(ipcheck.ipv4(2,5), (value: IPv4) => {
             return value.value.valueOf() >= 2 || value.value.valueOf() <= 5
+        }))
+    })
+})
+
+describe('IPv6', () => {
+   it('Generates arbitrary values between 0 and 2^128 - 1', () => {
+        fc.assert(fc.property(ipcheck.ipv6(), (value: IPv6) => {
+            return value.value.geq(0) || value.value.leq(Validator.IPV6_SIZE)
+        }))
+    })
+    it('Generates arbitrary values between min and max', () => {
+        fc.assert(fc.property(ipcheck.ipv6(BigInt(2), BigInt(5)), (value: IPv6) => {
+            return value.value.geq(2) || value.value.leq(5)
         }))
     })
 })
--- packages/ip-num-check/src/index.ts
+++ packages/ip-num-check/src/index.ts
@@ -1,20 +1,29 @@
 import fc from "fast-check"
-import {Asn, IPv4} from "ip-num";
+import {Asn, IPv4, IPv6, Validator} from "ip-num";
 import * as bigInt from "big-integer/BigInteger";
 
-const ipv4max = Math.pow(2,32) - 1
-
+const ipv4max = Validator.IPV4_SIZE.valueOf()
+const ipv6max = Validator.IPV6_SIZE.minus(1)
 const asn = (min:number=0, max:number=65535) => {
     return fc.integer({min, max}).map(value => Asn.fromNumber(value))
 }
 
-const ipv4 = (min:number=0,max:number=ipv4max) => {
+const ipv4 = (min:number=0, max:number=ipv4max) => {
     return fc.integer({min,max}).map(value => IPv4.fromBigInteger(bigInt(value)))
+}
+
+const ipv6 = (min:bigint=BigInt(0), max:bigint=BigInt(ipv6max.toString())) => {
+    return fc.bigUint({max}).filter(value => {
+        return value >= min
+    }).map(value => {
+        return IPv6.fromBigInteger(bigInt(value))
+    })
 }
 
 const ipcheck = {
     asn,
-    ipv4
+    ipv4,
+    ipv6
 }
 
 export default ipcheck;
